a65392e57f84540d78ba43ef4403d6d6bf50e85a
Sergey Durov <sergssd@yandex.com>
2020-11-01 15:19:31 +0500

    feat: add two solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
   
+  let parameters = {
+    repeatTimes: Number(options.repeatTimes),
+    separator: String(options.separator || '+'),
+    addition: String(options.addition),
+    additionRepeatTimes: Number(options.additionRepeatTimes),
+    additionSeparator: String(options.additionSeparator || '|')
+  }
+
+  let STR = '';
+  let result = [];
+
+  if (options.additionRepeatTimes != 0 && options.additionRepeatTimes != undefined) {
+    for (let i = 1; i <= parameters.additionRepeatTimes; i++) {
+      if (i == 1) STR += str;
+      STR += parameters.addition;
+      if (i == parameters.additionRepeatTimes) break;
+      STR += parameters.additionSeparator;
+    }
+  } else {
+    STR += str;
+  }
+
+  if (options.repeatTimes == undefined && options.additionRepeatTimes == undefined) return STR + options.addition;
+  if (options.repeatTimes == undefined) return STR;
+
+  for (let i = 1; i <= parameters.repeatTimes; i++) {
+    result.push(STR);
+    if (i == parameters.repeatTimes) break;
+    result.push(parameters.separator);
+  }
+
+  return result.join('');
+};
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,14 +1,32 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function transform(arr) {
   if (!Array.isArray(arr)) return new Throw('It is not Array');
   let arrCopy = arr.slice();
   let transArr = [];
   for (let i = 0; i < arrCopy.length; i++) {
-    if (arr[i] === '--discard-next') arrCopy.splice(i + 1, 1);
-    if (arr[i] === '--discard-prev') transArr.splice(-1, 1);
-    if (arr[i] === '--double-next') transArr.push(arrCopy[i + 1]);
-    if (arr[i] === '--double-prev') transArr.push(arrCopy[i - 1]);
+    if (arrCopy[i] === '--discard-next') {
+      if(i + 1 >= arrCopy.length) break;
+      arrCopy[i + 1] = "s0_disc";
+    } else if (arrCopy[i] === '--discard-prev') {
+      if(i - 1 < 0) continue;
+      transArr[i - 1] = "s0_disc";
+    } else if (arrCopy[i] === '--double-next') {
+      if(i + 1 > arrCopy.length) break;
+      transArr[i] = arrCopy[i + 1];
+    } else if (arrCopy[i] === '--double-prev') {
+      if(i - 1 < 0) continue;
+      transArr[i] = transArr[i - 1];
+    } else {
+      transArr.push(arrCopy[i])
+    }
   };
+
+  arrCopy = transArr.slice();
+  transArr = [];
+
+  arrCopy.forEach(item => {
+    if (item != 's0_disc' && item != undefined) transArr.push(item);
+  });
+  
   return transArr;
 };
+
